fce7d11c11398f036c85ce61e96f16010d801bd5
JohanSolanoMendez <106444972+JohanSolanoMendez@users.noreply.github.com>
2024-06-01 18:18:46 -0600

    Generation

---
+++ src/main/java/net/bizwiz/Blazing_elementsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bizwiz;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Blazing_elementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
